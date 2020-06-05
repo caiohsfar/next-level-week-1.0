cd34325cd446dc64158c1e767e2546c2402f7597
Caio <caiohenriquefarias16@gmail.com>
2020-06-04 21:38:03 -0300

    start items integration test

---
+++ backend/test/integration/routes/items.spec.ts
@@ -0,0 +1,33 @@
+import connection from '../../../src/database/connection'
+import supertest, { SuperTest } from 'supertest'
+import app from '../../../src/app'
+
+describe('Routes: items', () => {
+    let request: SuperTest<supertest.Test>
+
+    beforeAll(() => {
+        request = supertest(app)
+    })
+
+    beforeEach(async () => {
+        await connection.migrate.rollback()
+        await connection.migrate.latest()
+        await connection.seed.run()
+    })
+
+    afterEach(async () => {
+        await connection.migrate.rollback()
+    })
+
+    describe('GET /items', () => {
+        test('Should return an array of items', async (done) => {
+            request.get('/items')
+                .end((err, res) => {
+                    expect(res.status).toBe(200)
+                    expect(res.body.data).toBeDefined()
+                    done(err)
+                })
+        })
+    })
+})
+
