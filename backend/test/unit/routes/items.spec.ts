6d07a64752323cabe515e0c5584899716b5824b9
Caio <caiohenriquefarias16@gmail.com>
2020-06-10 21:56:35 -0300

    update items unit test

--- backend/test/unit/routes/items.spec.ts
+++ backend/test/unit/routes/items.spec.ts
@@ -1,30 +1,36 @@
 import sinon from 'sinon'
-import knex from '../../../src/database/connection'
+import app from '../../../src/app'
 import itemsController from '../../../src/controllers/items'
 import { Request, Response } from 'express'
+
 describe('Controller: Items', () => {
-    const knexMock = sinon.mock(knex)
     const defaultRequest = {
         params: {
 
         }
     } as Request
 
-    const itemsArray = [{
+    const defaultResponse = {
+        send: jest.fn(),
+        status: sinon.stub()
+    }
+
+    const itemsDbArray = [{
+        id: 1,
         title: "a-title",
         image_url: 'a-url'
     }]
+
     describe('index()', () => {
         it('Should return an array of items', async () => {
-            const response = {
-                send: jest.fn()
-            } as unknown as Response
+            const dbMock = sinon.mock(app.database)
+            dbMock.expects('select').once().withArgs('*').returnsThis()
+            dbMock.expects('from').once().withArgs('items').resolves(itemsDbArray)
 
-            knexMock.expects('select').once().withArgs('*').returnsThis()
-            knexMock.expects('from').once().withArgs('items').resolves(itemsArray)
+            await itemsController.index(defaultRequest, defaultResponse as unknown as Response)
 
-            await itemsController.index(defaultRequest, response)
-            expect(response.send).toHaveBeenCalledWith(
+            dbMock.verify()
+            expect(defaultResponse.send).toHaveBeenCalledWith(
                 expect.objectContaining({
                     data: expect.arrayContaining([{
                         title: expect.any(String),
@@ -32,6 +38,25 @@ describe('Controller: Items', () => {
                     }])
                 })
             )
+        })
+
+        describe('when throw an error', () => {
+            it('should return 500 as status code and an error message', async () => {
+                const dbMock = sinon.mock(app.database)
+
+                defaultResponse.status.withArgs(500).returnsThis()
+                dbMock.expects('select').once().withArgs('*').returnsThis()
+                dbMock.expects('from').once().withArgs('items').rejects(new Error('Could not get items'))
+
+                await itemsController.index(defaultRequest, defaultResponse as unknown as Response)
+
+                dbMock.verify()
+
+                sinon.assert.calledWith(defaultResponse.status, 500)
+                expect(defaultResponse.send).toHaveBeenCalledWith(
+                    expect.objectContaining({ message: expect.any(String) })
+                )
+            })
         })
     })
 
