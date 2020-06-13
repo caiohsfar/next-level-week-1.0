2ebdf3aff8fa3e5ceb7e484e07ad190a6ca77959
Caio <caiohenriquefarias16@gmail.com>
2020-06-13 13:09:31 -0300

    add point show function on controller

--- backend/src/controllers/points.ts
+++ backend/src/controllers/points.ts
@@ -1,12 +1,17 @@
-import { Request, Response } from "express";
+import { Request, Response, response } from "express";
 import Knex from "knex";
+import log4js from 'log4js';
+import logger from '../../src/utils/logger'
 
 class PointsController {
     private database: Knex
+    private logger: log4js.Logger
 
     constructor(database: Knex) {
         this.database = database
+        this.logger = logger.getLogger()
     }
+
     public async create(req: Request, res: Response) {
         const trx = await this.database.transaction()
 
@@ -45,7 +50,7 @@ class PointsController {
 
             return res.status(201).send({ data: { success: true } })
         } catch (error) {
-            //app.logger.error(error)
+            this.logger.error(error)
             trx.rollback()
 
             return res.status(500).send(new Error("aa"))
@@ -54,6 +59,29 @@ class PointsController {
     }
 
     public async show(req: Request, res: Response) {
+        const { id } = req.params
+        try {
+            const point = await this.database
+                .from('points')
+                .where('id', id)
+                .first()
+
+            if (!point) {
+                return res.status(400).send({ message: 'Point not found' })
+            }
+
+            const items = await this.database.from('items')
+                .join('point_item', 'items.id', '=', 'point_item.item_id')
+                .where('point_item.point_id', id)
+                .select('items.title')
+
+            return res.json({ data: { point, items } })
+
+        } catch (error) {
+            this.logger.error(error)
+            console.error(error)
+            res.status(500).send(new Error("Server error"))
+        }
     }
 }
 
