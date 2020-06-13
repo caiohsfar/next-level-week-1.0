65c8226e43d76e593a6368797da78b2c63652b14
Caio <caiohenriquefarias16@gmail.com>
2020-06-13 13:09:21 -0300

    add point :id router

--- backend/src/routes/points.ts
+++ backend/src/routes/points.ts
@@ -6,5 +6,6 @@ const pointsRouter = express.Router()
 
 const pointsController = new PointsController(database)
 pointsRouter.post('/', (req, res) => pointsController.create(req, res))
+pointsRouter.get('/:id', (req, res) => pointsController.show(req, res))
 
 export default pointsRouter
