85f5156e3b93ed457035f6fe2f04ac3df891c20e
Caio <caiohenriquefarias16@gmail.com>
2020-06-12 22:49:12 -0300

    add show() on point test

--- backend/test/unit/routes/points.spec.ts
+++ backend/test/unit/routes/points.spec.ts
@@ -1,10 +1,13 @@
 import sinon from 'sinon'
 import app from '../../../src/app'
-import pointsController from '../../../src/controllers/points'
-import { Request, Response, response } from 'express'
+import PointsController from '../../../src/controllers/points'
+import { Request, Response } from 'express'
+import knexMock from '../../../src/database/mocks/knex'
 import Sinon from 'sinon'
+import Knex from 'knex'
 
 describe('Controller: Points', () => {
+
     const defaultRequest = {
         params: {
 
@@ -27,20 +30,27 @@ describe('Controller: Points', () => {
         uf: 'PE',
         items: [2]
     }
+
+    const fakePoint = { ...newPointBody, image: 'a-fake-image' }
+    delete fakePoint.items
+
     const fakePointItems = [
         {
             item_id: 2,
             point_id: 1
         }
     ]
+    const fakeItem = {
+        id: 1,
+        image: 'image',
+        title: 'title'
+    }
 
     describe('create()', () => {
         it('Should create an point', async () => {
             const request = { body: newPointBody } as Request
-            const dbMock: Sinon.SinonMock = sinon.mock(app.database)
+            const dbMock: Sinon.SinonMock = sinon.mock(knexMock)
 
-            const finalPointToInsert = { ...newPointBody, image: 'a-fake-image' }
-            delete finalPointToInsert.items
 
             // transaction
 
@@ -50,7 +60,7 @@ describe('Controller: Points', () => {
 
             // insert point
             dbMock.expects('from').once().withArgs('points').returnsThis()
-            dbMock.expects('insert').once().withArgs(finalPointToInsert).resolves([1])
+            dbMock.expects('insert').once().withArgs(fakePoint).resolves([1])
 
             // insert point_item
             dbMock.expects('from').once().withArgs('point_item').returnsThis()
@@ -61,6 +71,7 @@ describe('Controller: Points', () => {
             // Atribuir comportamento ao stub
             defaultResponse.status.withArgs(201).returnsThis()
 
+            const pointsController = new PointsController(knexMock as unknown as Knex)
             await pointsController.create(request, defaultResponse as unknown as Response)
 
             dbMock.verify()
@@ -79,7 +90,7 @@ describe('Controller: Points', () => {
             const request = { body: newPointBody } as Request
             defaultResponse.status.withArgs(500).returnsThis()
 
-            const dbMock: Sinon.SinonMock = sinon.mock(app.database)
+            const dbMock: Sinon.SinonMock = sinon.mock(knexMock)
 
             dbMock.expects('transaction').once().resolvesThis()
 
@@ -90,8 +101,8 @@ describe('Controller: Points', () => {
 
             dbMock.expects('rollback').once().resolves()
 
+            const pointsController = new PointsController(knexMock as unknown as Knex)
             await pointsController.create(request, defaultResponse as unknown as Response)
-
             dbMock.verify()
             dbMock.restore()
 
@@ -101,4 +112,48 @@ describe('Controller: Points', () => {
             )
         });
     })
+
+    describe('show()', () => {
+        it('Should return a point and its items passing an point id', async () => {
+            const pointId = 'point-id'
+            const dbMock: Sinon.SinonMock = sinon.mock(knexMock)
+            const request = { params: { id: pointId } } as unknown as Request
+
+
+            const response = {
+                json: jest.fn()
+            }
+            // resgatar o point
+            dbMock.expects('from').once().withArgs('points').returnsThis()
+            dbMock.expects('where').once().withArgs('id', pointId).returnsThis()
+            dbMock.expects('first').once().resolves(fakePoint)
+
+            dbMock.expects('from').once().withArgs('items').returnsThis()
+            dbMock.expects('join')
+                .once()
+                .withArgs(
+                    'point_items',
+                    'items.id',
+                    '=',
+                    'point_item.item.id',
+                    pointId
+                )
+                .returnsThis()
+
+            dbMock.expects('where').once().withArgs('point_item.point_id', pointId)
+            dbMock.expects('select').once().withArgs('items.title').resolves([fakeItem])
+
+            const pointsController = new PointsController(knexMock as unknown as Knex)
+            await pointsController.show(request as Request, response as unknown as Response)
+
+            expect(response.json).toBeCalledWith(
+                expect.objectContaining({
+                    point: expect.objectContaining({
+                        fakePoint
+                    }),
+                    items: expect.arrayContaining([fakeItem])
+                }))
+        })
+    })
+
 })
