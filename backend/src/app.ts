655d4dd47694fbca8ce00fb60a8f523088104e48
Caio <caiohenriquefarias16@gmail.com>
2020-06-13 13:06:58 -0300

    remove logger from app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,17 +3,11 @@ import cors from 'cors'
 import bodyParser from 'body-parser'
 import routes from "./routes";
 import path from 'path'
-import Knex from 'knex';
-import connection from '../src/database/connection'
-import log4js from 'log4js';
 
 class App {
     public server: express.Application
-    public logger: log4js.Logger
-
     public constructor() {
         this.server = express()
-        this.logger = this.setUpAndGetLogger()
         this.setupMiddlewares()
         this.setupRoutes()
     }
@@ -28,16 +22,6 @@ class App {
         this.server.use(routes)
         this.server.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
     }
-
-    private setUpAndGetLogger(): log4js.Logger {
-        log4js.configure({
-            appenders: { app: { type: "file", filename: "app.log" } },
-            categories: { default: { appenders: ["app"], level: "error" } }
-        });
-
-        return log4js.getLogger()
-    }
-
 }
 
 export default new App()
