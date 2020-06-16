8079c6316e5547bda7550f851bac9308350e68de
Caio Farias <caiohenriquefarias16@gmail.com>
2020-06-15 21:44:55 -0300

    add GET /points on routes

--- backend/src/routes/points.ts
+++ backend/src/routes/points.ts
@@ -5,7 +5,8 @@ import database from '../database/connection'
 const pointsRouter = express.Router()
 
 const pointsController = new PointsController(database)
-pointsRouter.post('/', (req, res) => pointsController.create(req, res))
+pointsRouter.get('/', (req, res) => pointsController.index(req, res))
 pointsRouter.get('/:id', (req, res) => pointsController.show(req, res))
+pointsRouter.post('/', (req, res) => pointsController.create(req, res))
 
 export default pointsRouter
